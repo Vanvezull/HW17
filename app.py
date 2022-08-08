651459dc6d96e843ba581c99e817667405814f9d
Vanvezull <Amadeaus1990@gmail.com>
2022-08-08 19:55:30 +0300

    Commit 2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 
-from flask import Flask, request
+from flask import request
 from flask_restx import Api, Resource
 from config import app
 from models import *
@@ -23,11 +23,42 @@ class MoviesView(Resource):
             return MovieSchema(many=True).dump(Movie.query.filter(Movie.genre_id == genre_id).all()), 200
         return MovieSchema(many=True).dump(Movie.query.all()), 200
 
+    def post(self):
+        req_json = request.json
+        new_movie = Movie(**req_json)
+
+        with db.session.begin():
+            db.session.add(new_movie)
+        return "", 201
 
 @movie_ns.route('/<int:id>')  # Представление для получения фильма по id
 class MovieView(Resource):
     def get(self, id):
         return MovieSchema().dump(Movie.query.get(id)), 201
+
+    def put(self, id):
+        movie = db.session.query(Movie).get(id)
+        req_json = request.json
+
+        movie.title = req_json.get("title")
+        movie.description = req_json.get("description")
+        movie.trailer = req_json.get("trailer")
+        movie.year = req_json.get("year")
+        movie.rating = req_json.get("rating")
+        movie.genre_id = req_json.get("genre_id")
+        movie.director_id = req_json.get("director_id")
+
+        db.session.add(movie)
+        db.session.commit()
+
+        return "", 204
+
+    def delete(self, id):
+        movie = db.session.query(Movie).get(id)
+
+        db.session.delete(movie)
+        db.session.commit()
+        return ""
 
 
 @director_ns.route('/')  # Представление для получения всех режиссеров
